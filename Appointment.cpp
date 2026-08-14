f8976c0996b54d77de5ef2f3670642e5ab6ccb69
KenSei <haozheng00910@gmail.com>
2026-08-15 02:09:23 +0800

    updated case 1

--- Appointment.cpp
+++ Appointment.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Logo
-int main() {
+void logo() {
 	cout << R"(                                                                                                                                                                             
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####  
                                                                          
@@ -23,7 +23,10 @@ int main() {
                                                                          
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####        
                                                                          )" << endl;
-	// Menu
+}
+
+// Menu
+void menu() {
 	cout << "Welcome to the Appointment Scheduler!\n" << endl;
 	cout << "Please select an option from the menu below:" << endl;
 	cout << "1. View All Appointment" << endl;
@@ -31,38 +34,116 @@ int main() {
 	cout << "3. Cancel Appointment" << endl;
 	cout << "4. Reschedule Appointment" << endl;
 	cout << "5. View Staff Schedule" << endl;
-	cout << "6. Exit\n" << endl;
+	cout << "6. Appointment Marking" << endl;
+	cout << "7. Exit\n" << endl;
+}
+
+//define timeslot set
+struct Timeslot {
+	int id;
+	string time;
+	bool isBooked;
+	string staffName;
+};
+
+//timeslot
+void displayAppointment(const Timeslot schedule[], int size) {
+
+	const string RESET = "\033[0m";
+	const string RED = "\033[31m";
+	const string GREEN = "\033[32m";
+
+	//separator
+	string separator = "+" + string(5, '-') + "+" + string(21, '-') + "+" + string(11, '-') + "+" + string(17, '-') + "+";
+
+	//header
+	cout << separator << endl;
+	cout << "| " << left << setw(4) << "ID"
+		<< "| " << setw(20) << "Time Slot"
+		<< "| " << setw(10) << "Status"
+		<< "| " << setw(16) << "Staff" << "|" << endl;
+	cout << separator << endl;
+
+	//details
+	for (int i = 0; i < size; i++) {
+		string statusAppointment = schedule[i].isBooked ? "Booked" : "Available";
+		string statuscolor = schedule[i].isBooked ? RED : GREEN;
+		string staffname = schedule[i].isBooked ? schedule[i].staffName : "-";
+
+		cout << "| " << left << setw(4) << schedule[i].id
+			 << "| " << setw(20) << schedule[i].time
+			 << "| " << statuscolor << setw(10) << statusAppointment << RESET
+			 << "| " << setw(16) << staffname << "|" << endl;
+	}
+	cout << separator << endl;
+}
+
+//total slots
+const int TOTAL_SLOTS = 7;
+
+int main() {
+
+	//details
+	Timeslot schedule[TOTAL_SLOTS] = {
+		{1, "09:00 AM - 10:00 AM", false, ""},
+		{2, "10:00 AM - 11:00 AM", true,  "John Tan"},
+		{3, "11:00 AM - 12:00 PM", false, ""},
+		{4, "01:00 PM - 02:00 PM", true,  "Alice Lim"},
+		{5, "02:00 PM - 03:00 PM", false, ""},
+		{6, "03:00 PM - 04:00 PM", false, ""},
+		{7, "04:00 PM - 05:00 PM", false, ""}
+	};
+
+	int option;
+
+	//looping menu
+	do
+	{
+
+	logo();
+	menu();
 
 	// Get user input
-	int option;
 	cin >> option;
 
-	switch (option) {
-	case 1:
-		cout << "You selected: View All Appointment" << endl;
-		for (int i = 0; i < 5; i++) {
-			cout << "Appointment " << i + 1 << ": [Details of appointment]" << endl;
-
+		switch (option) {
+		case 1:
+			cout << "You selected: View All Appointment" << endl;
+			displayAppointment(schedule, TOTAL_SLOTS);
+			break;
+		case 2:
+			cout << "You selected: Create a New Appointment" << endl;
+			break;
+		case 3:
+			cout << "You selected: Cancel Appointment" << endl;
+			break;
+		case 4:
+			cout << "You selected: Reschedule Appointment" << endl;
+			break;
+		case 5:
+			cout << "You selected: View Staff Schedule" << endl;
+			break;
+		case 6:
+			cout << "You selected: Appoinment Marking" << endl;
+			break;
+		case 7:
+			cout << "Exiting the program. Goodbye!" << endl;
+			break;
+		default:
+			cout << "Invalid option. Please select a valid option from the menu." << endl;
+			break;
 		}
-		break;
-	case 2:
-		cout << "You selected: Create a New Appointment" << endl;
-		break;
-	case 3:
-		cout << "You selected: Cancel Appointment" << endl;
-		break;
-	case 4:
-		cout << "You selected: Reschedule Appointment" << endl;
-		break;
-	case 5:
-		cout << "You selected: View Staff Schedule" << endl;
-		break;
-	case 6:
-		cout << "Exiting the program. Goodbye!" << endl;
-		break;
-	default:
-		cout << "Invalid option. Please select a valid option from the menu." << endl;
-	}
+		//check option and stop the progress to show menu details, clear the number the looping before
+		if (option != 7) {
+			cout << "\nPress Enter to return to the menu...";
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin.get();
+		}
+
+	} while (option != 7);
 
     return 0;
 }
+
+
+
